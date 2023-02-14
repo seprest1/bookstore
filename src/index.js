991e9ceddc9ac2dd978a4e07fa0442b2d98d40f2
Sarah Preston <seprest@gmail.com>
2023-02-14 09:40:09 -0600

    basic setUp

--- src/index.js
+++ src/index.js
@@ -2,16 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+/// redux ///
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+// saga //
+import createSagaMiddleware from 'redux-saga'
+import { put, takeEvery, select } from 'redux-saga/effects';
+import axios from 'axios';
+
+
+//////////////////////// reducers //////////////////////////
+
+
+//////////////////////// sagas //////////////////////////
+function* rootSaga() {
+};
+
+
+/////////////////////// store //////////////////////////
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  combineReducers({
+ 
+  }),
+  applyMiddleware(sagaMiddleware, logger),
+);
+
+sagaMiddleware.run(rootSaga)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
