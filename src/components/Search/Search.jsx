83715bfa4fc9f5e7eceb98e267950092ca27dee2
Sarah Preston <seprest@gmail.com>
2023-02-14 14:09:06 -0600

    language filter works

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -5,28 +5,52 @@ import { Grid, TextField, InputLabel, MenuItem, FormControl, Select, Button } fr
 
 function Search () {
 
+    const dispatch = useDispatch();
+
     const [search, setSearch] = useState('');
     const books = useSelector (store => store.books);
+
+    //searches through json data for authors/titles/years matching
     const getResults = () => {
         const searchTerms = search.toLowerCase().split(' ').filter(term => term.trim() !== ''); //splits search terms into individual words
         console.log(searchTerms);
 
-        const results = [];
+        const matches = [];
         searchTerms.forEach(term => 
             books.filter(book => 
                 (book.title.toLowerCase().includes(term) || //searches through book titles
                 book.author.toLowerCase().includes(term) || //searches through book authors
                 books.year === Number(term)) //searches through publication year
-                && results.push(book)));
-        setSearch('');
-        console.log(results);
+                && matches.push(book)));
+
+        const results = matches.filter(book => book.language.toLowerCase().includes(filter)); //if user wants to filter by language
+        dispatch({ type: 'SET_RESULTS', payload: results}); //sends results to redux store
+        setSearch(''); //clear input
     };
 
+    //allows user to press 'enter' to activate search
+    const handleKeyPress = e => {
+        if (e.key === "Enter") {
+          getResults();
+        }
+    };
+
+    //filters json data for specific language without searching
     const [filter, setFilter] = useState('');
-    const results = useSelector (store => store.books);
-    const filterResults = () => {
-    }
-    
+    const results = useSelector (store => store.results);
+    const filterResults = (e) => {
+        setFilter(e.target.value);
+        console.log(e.target.value);
+        console.log(results);
+
+        if (results.length === 0){
+            const languageMatches = books.filter(book => book.language.toLowerCase().includes(e.target.value));
+            dispatch({ type: 'SET_RESULTS', payload: languageMatches});
+        }
+    };
+
+    const languages = ['English', 'Italian', 'French', 'Spanish', 'German', 'Portuguese', 'Russian', 'Greek', 'Norwegian', 'Japanese', 'Sanskrit', 'Icelandic', 'Chinese', 'Swedish', 'Persian', 'Arabic', 'Latin'];
+
     return (
         <Grid container id="search" spacing={2} sx={{display: 'flex', flexDirection: 'row', margin: '10% 0 0 0', gap: '2%', justifyContent: 'center'}}>
             <Grid item xs={5}>
@@ -37,7 +61,9 @@ function Search () {
                     size="small"
                     color="secondary"
                     value={search}
-                    onChange={(e) => setSearch(e.target.value)}>
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyPress={handleKeyPress}
+                    >
                 </TextField>
             </Grid>
             <Grid item xs={2}>
@@ -48,12 +74,10 @@ function Search () {
                         value={filter}
                         label="English"
                         color="secondary"
-                        onChange={(e) => setFilter(e.target.value)}
+                        onChange={filterResults}
                         size="small"
-                        >
-                            <MenuItem value={'English'}>English</MenuItem>
-                            <MenuItem value={'Italian'}>Italian</MenuItem>
-                            <MenuItem value={'French'}>French</MenuItem>
+                        >                  //creates select options from language array
+                            {languages.map(language =>  <MenuItem value={language.toLowerCase()}>{language}</MenuItem>)}
                         </Select>
                 </FormControl>
             </Grid>
