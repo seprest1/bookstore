b8058c0b4436df58efa3febd99ba9b6846eb8258
Sarah Preston <seprest@gmail.com>
2023-02-14 09:57:35 -0600

    set up components and basic elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import './App.css';
+//components
+import Header from './components/Header/Header';
+import Search from './components/Search/Search';
+import Results from './components/Results/Results';
 
 function App() {
-
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch({ type: 'FETCH_BOOKS' });
+  }, []);
+ 
   return (
     <div className="App">
+      <Header></Header>
+      <Search></Search>
+      <Results></Results>
     </div>
   );
 }
---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,9 @@
+function Details () {
+    return (
+        <div id="details">
+           Here are the details!
+        </div>
+    )
+};
+
+export default Details;
